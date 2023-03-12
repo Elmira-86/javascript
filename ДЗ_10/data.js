a78b9b592c1ac44868ced18c9f4ddadb9d680af0
Elmira-86 <120679545+Elmira-86@users.noreply.github.com>
2023-03-12 22:12:35 +0300

    Add files via upload

---
+++ ДЗ_10/data.js
@@ -0,0 +1,11 @@
+const dataInfo = `[
+  {
+    "activity": "Bake a pie with some friends",
+    "type": "cooking",
+    "participants": 3,
+    "price": 0.3,
+    "link": "https://www.boredapi.com/api/activity",
+    "key": "3141592",
+    "accessibility": 0.3
+  }
+]`
